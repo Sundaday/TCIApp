906767690c8e35db984bd793fb343b0d10110dad
Sundaday <john.koch.pro@gmail.com>
2021-09-06 18:09:02 +0200

    placement

--- pages/placement.js
+++ pages/placement.js
@@ -1,6 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
 
 function placement(){
     return <h1>ici seront affiché les stats des précédents raids</h1>
 }
 
-export default raid
+export default placement
