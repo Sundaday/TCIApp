e2604a59d81c851f60356c07df739e4401272b44
Sundaday <john.koch.pro@gmail.com>
2021-09-10 18:51:15 +0200

    fatMaj

--- pages/api/create.js
+++ pages/api/create.js
@@ -2,16 +2,42 @@
 
 import prisma from "../../lib/prisma";
 
+
+
 export default async (req, res) => {
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method not allowed' });
     }
 
-    try {
+    
         const { user } = req.body
-        const savedUser = await prisma.uSER.create({data:user})
-        res.status(200).json({ savedUser})
-    } catch (error) {
-        res.status(400).json({ message: 'Something went wrong' })
-    }
+        
+        const savedUser = await prisma.user.create({
+            data: {
+                pseudo:user,
+                mot_de_passe:user,
+                email:user,
+                guilde:user,
+                team: {
+                    create: {
+                        personnage:team,
+                        degat_max:team,
+                        element_team:team
+                    },
+                },
+            },
+            include: {
+                team: true,
+            },
+        })
+        console.log(savedUser)
+        res.status(200).json({ savedUser })
+    
+
+    //export async function getServerSideProps() {
+    //    const users = await prisma.uSER.findMany();
+    //    return {
+    //        props: { initialUsers: users },
+    //    }
+    //}
 }
--- pages/formulaire.js
+++ pages/formulaire.js
@@ -1,4 +1,3 @@
-import prisma from "../lib/prisma.js"
 import Head from "next/head"
 import { useState } from "react"
 import { Container, Form, Header } from "semantic-ui-react"
@@ -6,79 +5,113 @@ import 'semantic-ui-css/semantic.min.css';
 import { fetcher } from "../utils/fetcher.js"
 import React from 'react'
 
-export async function getServerSideProps(){
-    const users = await prisma.uSER.findMany();
-   return{
-       props: { initialUsers: users }, 
-    }
-}
-
-function formulaire(){
+function formulaire() {
     const [users, setUsers] = useState("")
     const [pseudo, setPseudo] = useState("")
     const [mot_de_passe, setMot_de_passe] = useState("")
     const [email, setEmail] = useState("")
     const [guilde, setGuilde] = useState("")
-
+    const [teams, setTeams] = useState("")
+    const [personnage, setPersonnage] = useState("")
+    const [degat_max, setDegat_max] = useState("")
+    const [element_team, setElement_team] = useState("")
     return (
         <>
-        <Head>
-            <title>formulaire Equilibrage</title>
-            <link 
-                rel="stylesheet" 
-                href="/logotci.png" 
-            />
-        </Head>
-        <Container style ={{margin:20}}>
-            <Header as="h3">
-                C'est ici qu'on renseigne ses scores !
+            <Head>
+                <title>formulaire Equilibrage</title>
+                <link
+                    rel="stylesheet"
+                    href="/logotci.png"
+                />
+            </Head>
+            <Container style={{ margin: 20 }}>
+                <Header as="h3">
+                    C'est ici qu'on renseigne ses scores !
             </Header>
-            <Form onSubmit={async () => {
-                const body ={
-                    pseudo,
-                    mot_de_passe,
-                    email,
-                    guilde
-                }
-                    
-                await fetcher("/api/create", { user: body }),
-                await setUsers([...users,body]),              
-                setPseudo(""),
-                setMot_de_passe(""),
-                setEmail(""),
-                setGuilde("")
+                <Form onSubmit={async () => {
+                    const body = {
+                        pseudo,
+                        mot_de_passe,
+                        email,
+                        guilde
+                    }
 
-            }}> 
-            <Form.Group widths="equal">
-                <Form.Input 
-                    fluid 
-                    label="Pseudo IG"
-                    placeholder="Pseudo"
-                    value={pseudo}
-                    onChange={(e) => setPseudo(e.target.value)}
-                />
-                <Form.Input 
-                    fluid label="Mot de passe" 
-                    placeholder="Mot de passe"
-                    value={mot_de_passe}
-                    onChange={(e) => setMot_de_passe(e.target.value)}
-                /> 
-                <Form.Input 
-                    fluid label="Email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <Form.Input 
-                    fluid label="Guilde"
-                    placeholder="Guilde"
-                    value={guilde}
-                    onChange={(e) => setGuilde(e.target.value)}
-                />
-                </Form.Group>
-                <Form.Button>Submit</Form.Button>
-            </Form>
-        </Container>
+                    await fetcher("/api/create", { user: body }),
+                        await setUsers([...users, body]),
+                        setPseudo(""),
+                        setMot_de_passe(""),
+                        setEmail(""),
+                        setGuilde("")
+
+                }}>
+                    <Form.Group widths="equal">
+                        <Form.Input
+                            fluid
+                            label="Pseudo IG"
+                            placeholder="Pseudo"
+                            value={pseudo}
+                            onChange={(e) => setPseudo(e.target.value)}
+                        />
+                        <Form.Input
+                            fluid label="Mot de passe"
+                            placeholder="Mot de passe"
+                            value={mot_de_passe}
+                            onChange={(e) => setMot_de_passe(e.target.value)}
+                        />
+                        <Form.Input
+                            fluid label="Email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <Form.Input
+                            fluid label="Guilde"
+                            placeholder="Guilde"
+                            value={guilde}
+                            onChange={(e) => setGuilde(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Button>Submit</Form.Button>
+                </Form>
+                <Form onSubmit={async () => {
+                    const body = {
+                        personnage,
+                        degat_max,
+                        element_team
+
+                    }
+
+                    await fetcher("/api/create", { team: body }),
+                        await setTeams([...teams, body]),
+                        setPersonnage(""),
+                        setDegat_max(""),
+                        setElement_team("")
+
+                }}>
+                    <Form.Group widths="equal">
+                        <Form.Input
+                            fluid
+                            label="Personnage"
+                            placeholder="Personnage"
+                            value={personnage}
+                            onChange={(e) => setPersonnage(e.target.value)}
+                        />
+                        <Form.Input
+                            fluid label="degat max"
+                            placeholder="degat max"
+                            value={degat_max}
+                            onChange={(e) => setDegat_max(e.target.value)}
+                        />
+                        <Form.Input
+                            fluid label="element team"
+                            placeholder="element"
+                            value={element_team}
+                            onChange={(e) => setElement_team(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Button>Submit</Form.Button>
+                </Form>
+            </Container>
         </>
     )
 }
