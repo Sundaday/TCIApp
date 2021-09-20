b0ac09f4217f74826fcb482a342028740af41e67
Sundaday <john.koch.pro@gmail.com>
2021-09-20 15:18:09 +0200

    maj

--- pages/equilibrage.js
+++ pages/equilibrage.js
@@ -1,6 +1,8 @@
+import styles from '../styles/Home.module.css'
+
 function equilibrage(){
     return(
-        <h1>Page où se trouvera l'équilibrage</h1>
+        <h1 className={styles.description}>Page où se trouvera l'équilibrage</h1>
     )
 }
 
