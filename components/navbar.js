5fd2df46d9a9651b164bc852bf79b1d6852e26ec
Sundaday <john.koch.pro@gmail.com>
2021-09-20 14:55:10 +0200

    Biggest rollback to win

--- components/navbar.js
+++ components/navbar.js
@@ -1,15 +1,19 @@
 import Link from 'next/link'
 import 'semantic-ui-css/semantic.min.css';
+import styles from '../styles/Home.module.css'
+
 
 const Navbar = () =>{
 
     return (
         <nav>
-            <h1 className="homeButton">
-                <Link href='/'>
-                    TCIapp
-                </Link>
-            </h1>      
+            <div className="logo">
+                <h1 className={styles.homeButton}>
+                    <Link href='/'>
+                        TCIapp
+                    </Link>
+                </h1> 
+            </div>
         </nav>
     )
 }
--- pages/temperance.js
+++ pages/temperance.js
@@ -1,90 +1,73 @@
+import FormTeam from '../components/formTeam'
 import prisma from '../lib/prisma'
-import { Container, Divider, Header, Tab, Table } from "semantic-ui-react"
+import { Divider, Table } from "semantic-ui-react"
+import Styles from '../styles/Home.module.css'
 
-export async function getServerSideProps() {
-    
-    const users = await prisma.user.findMany()
-    const teams = await prisma.team.findMany()
+
+export async function getStaticProps() {
+
+    const users = await prisma.user.findMany({
+       
+        include: {
+            team: {
+                select: {
+                    personnage: true,
+                    element_team: true,
+                    degat_max: true
+                }
+            }
+        }
+    })
+    //const team = users[0].team[0];
+    //console.dir(team.degat_max)
+    //console.dir(users,{depth:null})
+
     return {
-        props: { users, teams }
+        props: { users }
     }
 }
 
-function temperance({ users, teams }) {
+
+function imperatrice({ users }) {
     return (
         <>
-            <header>
-                <h1>
-                    Guilde Tempérance
-                </h1>
-            </header>
+            <h1 >Guilde Tempérance</h1>
             <Divider horizontal>Team</Divider>
-            <Table basic="very" celled collapsing >
-                <Table>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>Pseudo</Table.HeaderCell>
-                            <Table.HeaderCell>Guilde</Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {users.map((users, index) =>
-                        <Table.Row key={index}>
+            <Table basic="very" celled collapsing className={Styles.formForm}>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Pseudo</Table.HeaderCell>
+                        <Table.HeaderCell>Guilde</Table.HeaderCell>
+                        <Table.HeaderCell>Element</Table.HeaderCell>
+                        <Table.HeaderCell>Personnage</Table.HeaderCell>
+                        <Table.HeaderCell>Dégâts Max</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {users.map((user, index) => {
+                        console.log('user', user)
+                        return (<Table.Row key={index}>
                             <Table.Cell>
-                                <Header>
-                                    <Header.Content>
-                                        {users.pseudo}
-                                    </Header.Content>
-                                </Header>
+                                {user?.pseudo || '-'}
                             </Table.Cell>
                             <Table.Cell>
-                                <Header>
-                                    <Header.Content>
-                                        {users.guilde}
-                                    </Header.Content>
-                                </Header>
-                            </Table.Cell>
-                        </Table.Row>)}
-                    </Table.Body>
-                </Table>
-                <Table>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>Element</Table.HeaderCell>
-                            <Table.HeaderCell>Personnage</Table.HeaderCell>
-                            <Table.HeaderCell>Dégâts Max</Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {teams.map((teams, index) =>
-                        <Table.Row key={index}>
-                            <Table.Cell>
-                                <Header>
-                                    <Header.Content>
-                                        {teams.element_team}
-                                    </Header.Content>
-                                </Header>
+                                {user?.guilde || '-'}
                             </Table.Cell>
                             <Table.Cell>
-                                <Header>
-                                    <Header.Content>
-                                        {teams.personnage}
-                                    </Header.Content>
-                                </Header>
+                                {user?.team[0].element_team || '-'}
                             </Table.Cell>
                             <Table.Cell>
-                                <Header>
-                                    <Header.Content>
-                                        {teams.degat_max}
-                                    </Header.Content>
-                                </Header>
+                                {user?.team[0].personnage || '-'}
                             </Table.Cell>
-                        </Table.Row>)}
-                    </Table.Body>
-                </Table>
+                            <Table.Cell>
+                                {user?.team[0].degat_max || '-'}
+                            </Table.Cell>
+                        </Table.Row>)
+                    })}
+                </Table.Body>
             </Table>
         </>
     )
 }
 
-export default temperance;
+export default imperatrice;
